f99bce1533950d86ee18ad4a512b487c56472272
Vlad Levitin <levitinvlad@hotmail.com>
2023-04-24 05:06:34 +0200

    new commit

--- Clustering/MA_model.py
+++ Clustering/MA_model.py
@@ -1,9 +1,10 @@
 import os
 import pandas as pd
 import json
+import numpy as np
 
-def trainMA(X_train, Y_train, station):
-    moving_average = Y_train.rolling(window=3).mean().iloc[-1]
+def trainMA(X_train, Y_train, station, window_size=3):
+    moving_average = Y_train.rolling(window=window_size).mean().iloc[-1]
     print(f"Training for station {station}")
     return moving_average
 
@@ -34,7 +35,7 @@ def getAreaData(area, month):
 
 # Other functions remain the same
 
-def Test_train_month(pred_periods, month):
+def Test_train_month(pred_periods, month, window_size=10):
     """Implements the moving average prediction model for the given number of periods"""
 
     areas = getAreas(month)
@@ -61,17 +62,36 @@ def Test_train_month(pred_periods, month):
     
         Y_train = train_data["count"]
 
-        models[station] = trainMA(X_train, Y_train, station)
+        models[station] = trainMA(X_train, Y_train, station,window_size)
         print("Area ", areas.index(station), " has been trained")
         
 
+    # for i in range(0, pred_periods):
+    #     print(i)
+    #     for station in areas:
+    #         moving_average = models[station]
+    #         CO_pred[station].append(moving_average)
+    #         testX[station]["count_last_hour"][i + 1] = moving_average
+    
     for i in range(0, pred_periods):
         print(i)
         for station in areas:
             moving_average = models[station]
             CO_pred[station].append(moving_average)
+            
+            # Update the moving average with the actual value from the previous time step
+            if i > 0:
+                moving_average = (moving_average * (window_size - 1) + testY[station][i - 1]) / window_size
+                moving_average = (moving_average * (window_size - 1) + testY[station][i - 1]) / window_size
+
+                
             testX[station]["count_last_hour"][i + 1] = moving_average
-        
+            
+  
+    
+  
+            
+            
     with open(f'Clustering/results/{month}/CO_MA_norm_pred.json', 'w') as fp:
         json.dump(CO_pred, fp)
     with open(f'Clustering/results/{month}/testY_norm.json', 'w') as fp:
